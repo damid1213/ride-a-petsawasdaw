cba15c4da025cf00ed83caf355c4fd1321a55961
damid1213 <valorantaccsav@gmail.com>
2026-09-25 14:44:28 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -18,7 +18,7 @@
 -- ── Configuration ────────────────────────────────────────────────────
 local CONFIG = {
     BASE_URL    = "https://raw.githubusercontent.com/damid1213/ride-a-petsawasdaw/refs/heads/main/src/",
-    VERSION_URL = "https://raw.githubusercontent.com/LostInSyntaxx/RideAPet/main/version.txt",
+    VERSION_URL = "https://raw.githubusercontent.com/damid1213/ride-a-petsawasdaw/refs/heads/main/",
     CACHE_DIR   = "Vanguard",
     NAMESPACE   = "EggsESP",
 
